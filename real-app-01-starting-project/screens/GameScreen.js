a30cfc35afd054cb79cd127da5be5c7f7225d139
sancoLgates <sancolgates1@gmail.com>
2021-08-13 17:30:07 +0700

    68. Adding Game Features

---
+++ real-app-01-starting-project/screens/GameScreen.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+import NumberContainer from "../component/NumberContainer";
+import Card from "../component/Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randNum = Math.floor(Math.random() * (max - min)) + min;
+  if (randNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  }
+  return randNum;
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 99, props.userChoice)
+  );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: "75%",
+  },
+});
+
+export default GameScreen;
