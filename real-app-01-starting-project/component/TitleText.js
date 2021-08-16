36eee77f0a80508c057cc776b776ea316f7325b0
sancoLgates <sancolgates1@gmail.com>
2021-08-17 06:39:05 +0700

    80. Building a Custom Button Component

--- real-app-01-starting-project/component/TitleText.js
+++ real-app-01-starting-project/component/TitleText.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
-const BodyText = (props) => (
+const TitleText = (props) => (
   <Text style={{ ...styles.body, ...props.style }}>{props.children}</Text>
 );
 
 const styles = StyleSheet.create({
   body: {
-    fontFamily: "open-sans",
+    fontFamily: "open-sans-bold",
   },
 });
 
-export default BodyText;
+export default TitleText;
--- real-app-01-starting-project/screens/GameOverScreen.js
+++ real-app-01-starting-project/screens/GameOverScreen.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+
+import BodyText from "../component/BodyText";
+import TitleText from "../component/TitleText";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of Rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          // fadeDuration={1000}
+          // source={{
+          //   uri:
+          //     "https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_1280.jpg",
+          // }}
+          source={require("../assets/images/success.png")}
+          resizeMode="cover"
+        />
+      </View>
+      <BodyText style={styles.resultText}>
+        Your phone needed{" "}
+        <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+        guess the number{" "}
+        <Text style={styles.highlight}>{props.userNumber}</Text>
+      </BodyText>
+      {/* <Text numberOfLines={1} ellipsizeMode="tail">
+        This text will never wrap into a new line, instead it will be cut off
+        like this if it is too lon...
+      </Text> */}
+      {/* <BodyText>Number of Rounds: {props.roundsNumber}</BodyText> */}
+      {/* <BodyText>Number was: {props.userNumber}</BodyText> */}
       <Button title="New Game" onPress={props.onRestart} />
     </View>
   );
@@ -17,6 +42,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "grey",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  resultText: {
+    textAlign: "center",
+  },
+  highlight: {
+    color: "red",
+    // fontWeight: "900",
+    fontSize: 18,
+    fontFamily: "open-sans-bold",
   },
 });
 
