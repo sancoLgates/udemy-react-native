294b66172cf634b0e307b3f4b0458dc4e0485946
sancoLgates <sancolgates1@gmail.com>
2021-08-23 11:25:26 +0700

    118. Navigating Between Screens

--- meals-app/App.js
+++ meals-app/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, SafeAreaView, StyleSheet } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import MealsNavigator from "./navigation/MealsNavigator";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log("err", err)}
+      />
+    );
+  }
+
+  return <MealsNavigator />;
 }
+
+// const styles = StyleSheet.create({
+//   text: {
+//     fontFamily: "open-sans-bold",
+//     // fontSize: 20,
+//   },
+// });
