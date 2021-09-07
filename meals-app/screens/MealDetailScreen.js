e46b5c6c28ce4c68e54fd1b7dbc45a34b3e52ec4
sancoLgates <sancolgates1@gmail.com>
2021-09-07 14:01:02 +0700

    138. Adding MaterialBottomTabs

--- meals-app/screens/MealDetailScreen.js
+++ meals-app/screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
@@ -23,6 +25,24 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite!");
+          }}
+        ></Item>
+        <Item
+          title="Favorite"
+          iconName="ios-star-outline"
+          onPress={() => {
+            console.log("Mark as favorite!");
+          }}
+        ></Item>
+      </HeaderButtons>
+    ),
   };
 };
 
