814603e225980701da008bedd71bd49ed96b88dc
sancoLgates <sancolgates1@gmail.com>
2021-11-06 07:25:46 +0700

    169. Adding Touchable Components

--- shop-app/App.js
+++ shop-app/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import productsReducer from "./store/reducers/products";
+import ShopNavigator from "./navigation/ShopNavigator";
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View>
-      <Text>A new app!</Text>
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
 }
