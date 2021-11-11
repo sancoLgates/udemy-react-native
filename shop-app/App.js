a4d7d42e984ea9a609eb751d8b72370b83397264
sancoLgates <sancolgates1@gmail.com>
2021-11-11 10:29:48 +0700

    172. Adding Items to the Cart

--- shop-app/App.js
+++ shop-app/App.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+// import { AppLoading, Font } from "expo";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
@@ -12,7 +15,26 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log("err", err)}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
