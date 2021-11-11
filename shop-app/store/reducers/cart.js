4f96e1ac86f062756d62ed4a8a8ab278f8c5cd29
sancoLgates <sancolgates1@gmail.com>
2021-11-11 14:11:21 +0700

    173. Implementing Header Buttons

--- shop-app/store/reducers/cart.js
+++ shop-app/store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART } from "../acttions/cart";
+import CartItem from "../../models/cart-item";
 
 const initialState = {
   items: {},
@@ -11,6 +12,25 @@ export default (state = initialState, action) => {
       const addedProduct = action.product;
       const prodPrice = addedProduct.price;
       const prodTitle = addedProduct.title;
+
+      let updatedOrNewCartItem;
+
+      if (items[addedProduct.id]) {
+        // already have the item in the cart
+        updatedOrNewCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+        totalAmount: state.totalAmount + prodPrice,
+      };
   }
   return state;
 };
