f312ccd1d7703d9cf6c563b7fc3fff20a26b9c7f
sancoLgates <sancolgates1@gmail.com>
2021-11-15 05:50:05 +0700

    Section 9: Handling User Input

--- shop-app/components/shop/ProductItem.js
+++ shop-app/components/shop/ProductItem.js
@@ -8,6 +8,7 @@ import {
   TouchableNativeFeedback,
   Platform,
 } from "react-native";
+import Card from "../UI/Card";
 
 const ProductItem = (props) => {
   let TouchableComponent =
@@ -16,7 +17,7 @@ const ProductItem = (props) => {
       : TouchableOpacity;
 
   return (
-    <View style={styles.product}>
+    <Card style={styles.product}>
       <View style={styles.touchable}>
         <TouchableComponent onPress={props.onSelect} useForeground>
           <View>
@@ -31,19 +32,12 @@ const ProductItem = (props) => {
           </View>
         </TouchableComponent>
       </View>
-    </View>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
     height: 300,
     margin: 20,
   },
